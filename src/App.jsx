8ba1a80a69ecc67460064f1659a82948568ec784
NadiiaUa23 <hasiminka92@gmail.com>
2025-05-20 10:55:22 +0200

    test conect the movie DB

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 // import { useState } from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div>
-        <p>Hello</p>
+        <Routes>
+          <Route />
+          <p>Holla</p>
+          <Route />
+        </Routes>
       </div>
     </>
   );
