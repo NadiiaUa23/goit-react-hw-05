8c721dbf14d3398244cfddf89ecaf1de48f65119
Nadiia Tkachenko <135448886+NadiiaUa23@users.noreply.github.com>
2025-05-21 15:18:53 +0200

    Add comentario

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 // import { useState } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import {HomePege} from "../srs/peges/Homepege.jsc"
+// подключить peges
+import {useParams} from "react-router-dom";
+
 
 function App() {
   return (
     <>
       <div>
         <Routes>
-          <Route path="/" element={<HomePage />}/>
-          
+          <Route path="/" element={<HomePage />}/
           <Route path="/movies" element={<Movies />}/>
+         <Route path="*" element={<NotFound />}/>
         </Routes>
       </div>
     </>
